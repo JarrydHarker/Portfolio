f112b4e924bbb4f0d4117f1d08b182f0ac24fe78
JarrydHarker <st10209687@vcconnect.edu.za>
2025-03-05 10:59:18 +0200

    Added navbar logic Added prospective colour pallette Added basic syling

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const bioIndex = 0;
+const projectsIndex = 1;
+const profIndex = 2;
+
+const items = ["bioLink", "projectsLink", "profLink"]
+
+var bio;
+var projects;
+var prof;
+
+var sections;
+
+document.addEventListener("DOMContentLoaded", function() {
+    bio = document.getElementById("Biography");
+    projects = document.getElementById("Projects");
+    prof = document.getElementById("Professional");
+
+    sections = [bio, projects, prof];
+
+    showSection(bioIndex)
+
+    document.getElementById("bioLink").addEventListener("click", function() {
+        showSection(bioIndex);
+    });
+    
+    document.getElementById("projectsLink").addEventListener("click", function() {
+        showSection(projectsIndex);
+    });
+    
+    document.getElementById("profLink").addEventListener("click", function() {
+        showSection(profIndex);
+    });
+});
+
+
+
+// Function to show the selected section and hide others
+function showSection(index) {
+     for(let i = 0; i < sections.length; i++){
+        let currentTab = document.getElementById(items[i]);
+
+        if (i === index) {
+            sections[i].classList.remove("hidden"); // Show the section
+            
+            currentTab.classList.add("nav-item-selected");
+
+            let iframe = sections[i].querySelector("iframe");
+            if (iframe) iframe.style.display = "block";
+        } else {
+            sections[i].classList.add("hidden"); // Hide the section
+            currentTab.classList.remove("nav-item-selected");
+            
+            let iframe = sections[i].querySelector("iframe");
+            if (iframe) iframe.style.display = "none"; // Ensure the iframe is also hidden
+        }
+    }
+}
